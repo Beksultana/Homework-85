4ac9ab1c187e1fc23d6d1d8625a03598bf5632f1
sultan <aidaraliuulu@gmail.com>
2019-05-01 00:07:44 +0600

    In backand, created post request for artist

--- homework-85-back/app/artists.js
+++ homework-85-back/app/artists.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const ArtistSchema = require('../modules/Artist');
-const router = express.Router();
+const multer = require('multer');
+const config = require('../config');
+const nanoid = require('nanoid');
 
-
-router.get('/', (req, res) => {
-    ArtistSchema.find()
-       .then(result => res.send(result))
-       .catch(() => res.sendStatus(500))
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, config.uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, nanoid() + path.extname(file.originalname))
+    }
 });
 
+const upload = multer({storage});
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const artists = await ArtistSchema.find();
+        return res.send(artists);
+    }catch (e) {
+        return res.status(500).send(e)
+    }
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+    const artistData = req.body;
+
+    if (req.file) {
+        artistData.image = req.file.filename;
+    }
+
+    const artist = new ArtistSchema(artistData);
+    artist.save()
+        .then(result => res.send(result))
+        .catch(error => res.send(error))
+});
 
 module.exports = router;
