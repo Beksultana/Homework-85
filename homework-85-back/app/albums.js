26a417e82bc1065602415a32bbd48e0b24e41584
sultan <aidaraliuulu@gmail.com>
2019-05-01 00:25:09 +0600

    in backand, created post request for albums

--- homework-85-back/app/albums.js
+++ homework-85-back/app/albums.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const AlbumSchema = require('../modules/Album');
 const router = express.Router();
+const multer = require('multer');
+const config = require('../config');
+const nanoid = require('nanoid');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, config.uploadPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, nanoid() + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({storage});
 
 router.get('/', (req, res) => {
     AlbumSchema.find()
@@ -13,6 +27,18 @@ router.get('/:id', (req, res) => {
         .then(result => res.send(result))
         .catch(() => res.sendStatus(500))
 
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+    const albumData = req.body;
+    if (req.file) {
+        albumData.image = req.file.filename
+    }
+
+    const album = new AlbumSchema(albumData);
+    album.save()
+        .then(result => res.send(result))
+        .catch(error => res.send(error))
 });
 
 
